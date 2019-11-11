2b0f310061b0921fd409c76fc9ea125dad0afbba
rohitverma1999hack <rohitvermaavcbbk2011@gmail.com>
2019-11-11 20:15:03 +0530

    Add Profile Image For User

--- app/src/main/java/com/example/needhelp/Profile.java
+++ app/src/main/java/com/example/needhelp/Profile.java
@@ -5,18 +5,28 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.ContentResolver;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthSettings;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -25,7 +35,15 @@ import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.Objects;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+import static android.widget.Toast.*;
 
 public class Profile extends AppCompatActivity {
 
@@ -34,7 +52,7 @@ public class Profile extends AppCompatActivity {
     public TextView email;
     public TextView phone;
     public ImageView close, photo;
-    private ImageView addinfo;
+    private TextView addinfo;
     String phonee, datetime;
     private StorageReference postref;
     DatabaseReference reference;
@@ -50,9 +68,7 @@ public class Profile extends AppCompatActivity {
         name = findViewById(R.id.userrname);
         email = findViewById(R.id.emailid);
         phone = findViewById(R.id.phone_number);
-
         photo = findViewById(R.id.select_post_image);
-
         close = findViewById(R.id.close);
 
         addinfo = findViewById(R.id.upload_new);
@@ -61,7 +77,13 @@ public class Profile extends AppCompatActivity {
         photo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // openFileChooser();
+                openFileChooser();
+            }
+        });
+        addinfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                uploadimagetofirebasedatabase();
             }
         });
         close.setOnClickListener(new View.OnClickListener() {
@@ -72,7 +94,6 @@ public class Profile extends AppCompatActivity {
             }
         });
         reference = FirebaseDatabase.getInstance().getReference("Users").child(Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid());
-        postref = FirebaseStorage.getInstance().getReference().child("Profile Images");
 
         reference.addValueEventListener(new ValueEventListener() {
             @Override
@@ -93,5 +114,49 @@ public class Profile extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void uploadimagetofirebasedatabase() {
+        if(mImageUri == null){
+            makeText(this, "Please Select A Photo From Gallery", LENGTH_SHORT).show();
+        }else {
+            postref = FirebaseStorage.getInstance().getReference().child("Profile_Images").child(Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid());
+            final String s = mImageUri.toString();
+            postref.child(FirebaseAuth.getInstance().getCurrentUser().getUid()).putFile(mImageUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+
+                    Toast.makeText(Profile.this, "Please check your email for verification .....", Toast.LENGTH_SHORT).show();
+                    DatabaseReference database = FirebaseDatabase.getInstance().getReference("Users").child(FirebaseAuth.getInstance().getCurrentUser().getUid());
+                    database.child("imageURL").setValue(mImageUri.toString()).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            Toast.makeText(Profile.this,"Imageurl succesfully save",Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            });
+        }
+    }
+
+    private void openFileChooser() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent,0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 0 && resultCode == Activity.RESULT_OK && data != null){
+            mImageUri  = data.getData();
+            Bitmap bitmap = null;
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),mImageUri);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            photo.setImageBitmap(bitmap);
+        }
     }
 }
